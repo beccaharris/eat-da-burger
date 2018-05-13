ce55b57a73d0c0a28aaf5aacea41a37f906dcc4d
Becca Harris <beccalynharris@gmail.com>
2018-05-13 00:51:13 -0600

    ugh heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var methodOverride = require('method-override')
 
 var exphbs = require('express-handlebars');
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Get content from 'public' folder //
